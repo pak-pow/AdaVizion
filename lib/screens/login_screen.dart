68b33b31de21c811dfb1808161cf5d6879731a88
vince <paulag10987@gmail.com>
2026-02-17 20:53:25 +0800

    feat(main, logIn_screen):
    - Implemented log in logic on login_screen state, added "logo", added title with input state connecting to the variable
    - refactored the main.lib with skeleton code and add import for loginscreen state

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -7,24 +7,56 @@ class LoginScreen extends StatefulWidget {
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _idController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(vsync: this);
-  }
+  void login() {
+    String studentId = _idController.text;
+    String password = _passwordController.text;
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+    if (studentId == "2023-1234" && password == "password") {
+      print("LOGIN SUCCESS");
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("Welcome Back, Student!")));
+    } else {
+      print("LOGIN FAILED");
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("INVALID ID or PASSWORD"),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+    ;
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(Icons.school, size: 80, color: Colors.red),
+            const SizedBox(height: 20),
+
+            const Text(
+              "ADAVIZION",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+              ),
+            ),
+
+            const SizedBox(height: 40),
+          ],
+        ),
+      ),
+    );
   }
 }

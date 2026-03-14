1c5ac53c4511fe63b4aa5fd05347a59c20436c43
vince <paulag10987@gmail.com>
2026-03-14 21:00:25 +0800

    chore(dashboard): add app bar for eu venture

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -9,15 +9,13 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
-  
   // The `int _currentIndex = 0;` line declares an integer variable named `_currentIndex` and
   // initializes it with a value of 0. This variable is typically used to keep track of the current
   // index or position within a list or array of items. In this case, it might be used to determine
   // which screen to display from the `_screens` list based on the current index value.
   int _currentIndex = 0;
 
-  // The `final List<Widget> _screens` variable is an array that holds a list of Widgets. In this case,
-  // it contains five Widgets:
+  // The `final List<Widget> _screens` variable is an array that holds a list of Widgets.
   final List<Widget> _screens = [
     const Center(
       child: Text("Landmarks List Screen", style: TextStyle(fontSize: 24)),
@@ -33,6 +31,19 @@ class _DashboardScreenState extends State<DashboardScreen> {
 
   @override
   Widget build(BuildContext content) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[50],
+      appBar: AppBar(
+        title: const Text(
+          "EUventure",
+          style: TextStyle(
+            color: Color(0xFF800000),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 1,
+      ),
+    );
   }
 }

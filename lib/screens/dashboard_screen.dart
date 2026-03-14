70149d394d948a0f1d625bb792a6c5e1c8cdb50c
vince <paulag10987@gmail.com>
2026-03-14 20:39:49 +0800

    chore(tab): add placeholder screens in this case is the list and also an index to know which state it is

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -7,5 +7,24 @@ class DashboardScreen extends StatefulWidget {
   State<DashboardScreen> createState() => _DashboardScreenState();
 }
 
-class _DashboardScreenState extends State<DashboardScreen>{
+class _DashboardScreenState extends State<DashboardScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const Center(
+      child: Text("Landmarks List Screen", style: TextStyle(fontSize: 24)),
+    ),
+    const Center(child: Text("Quizzes Screen", style: TextStyle(fontSize: 24))),
+    const SizedBox(),
+
+    const Center(
+      child: Text("Rankings Screen", style: TextStyle(fontSize: 24)),
+    ),
+    const Center(child: Text("Profile Screen", style: TextStyle(fontSize: 24))),
+  ];
+
+  @override
+  Widget build(BuildContext content) {
+    return Scaffold();
+  }
 }

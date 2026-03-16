d5260ae3e9953d70d41960002261b48fa445e0cd
vince <paulag10987@gmail.com>
2026-03-16 19:25:33 +0800

    feat: add LandmarkScreen scaffold for new landmarks tab

---
+++ lib/screens/landmark_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LandmarkScreen extends StatelessWidget {
+  const LandmarkScreen({super.key});
+
+  final List<Map<String, dynamic>> landmarks = const [
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

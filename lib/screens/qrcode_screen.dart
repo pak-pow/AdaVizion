6f17cec190411125c4be3f74e4c869b7b1d1143e
Lapotski <kd.dequito@gmail.com>
2026-03-16 23:11:45 +0800

    feat: implemented qr code scanning to codebase

--- lib/screens/qrcode_screen.dart
+++ lib/screens/qrcode_screen.dart
@@ -10,8 +10,73 @@ class QRCodeScreen extends StatefulWidget {
 
 class _QRCodeScreenState extends State<QRCodeScreen> {
   // this allows us to control the camera and listen for QR code scans
-  final MobileScannerController _controller = MobileScannerController();
+  final MobileScannerController _cameraController = MobileScannerController();
   bool isScanning = true;
+
+  // function to allow to navigate to different screens based on the scanned QR code value
+  void _showOptionsDialog(String codeValue) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Landmark Detected!"),
+          content: Text(
+            "You have scanned: $codeValue\n\nWhat would you like to do?",
+          ),
+          actions: [
+            // This button allows user to go back to DashboardScreen
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.pop(context);
+              },
+              style: TextButton.styleFrom(foregroundColor: Colors.red),
+              child: const Text("Go to Dashboard"),
+            ),
+            // This button allows the user to proceed to Landmark Details Screen (not implemented yet)
+            // Temporarily reroutes to DashbordScreen with a print statement for demonstration purposes.
+            ElevatedButton(
+              onPressed: () async {
+                Navigator.pop(context);
+
+                await showDialog(
+                  context: context,
+                  barrierDismissible: false,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text("Landmark Fun Facts!"),
+                      content: Text("Navigating to Fun Facts with: $codeValue"),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          style: TextButton.styleFrom(
+                            foregroundColor: Colors.red,
+                          ),
+                          child: const Text("Close"),
+                        ),
+                      ],
+                    );
+                  },
+                );
+
+                if (mounted) {
+                  Navigator.pop(context);
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                foregroundColor: Colors.white,
+              ),
+              child: const Text("View Fun Facts"),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,6 +93,30 @@ class _QRCodeScreenState extends State<QRCodeScreen> {
         backgroundColor: Colors.white,
         elevation: 1,
       ),
+
+      body: MobileScanner(
+        controller: _cameraController,
+        onDetect: (capture) {
+          if (isScanning) {
+            final barcode = capture.barcodes.first;
+
+            if (barcode.rawValue != null) {
+              setState(
+                () => isScanning = false,
+              ); // this prevents multiple scans
+              _cameraController.stop(); // stops the cam after successful scan
+
+              _showOptionsDialog(barcode.rawValue!);
+            }
+          }
+        },
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _cameraController.dispose();
+    super.dispose();
   }
 }

ca2f0b4c83703522dc1ea39845d68f081876a95f
neophiles <mnv.tagle@gmail.com>
2026-04-14 20:21:23 +0800

    feat(db): add img_path to Student, Landmark, and Achievement models

---
+++ backend/prisma/migrations/20260414122030_add_img_path_to_models/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "achievements" ADD COLUMN     "img_path" VARCHAR(255);
+
+-- AlterTable
+ALTER TABLE "landmarks" ADD COLUMN     "img_path" VARCHAR(255);
+
+-- AlterTable
+ALTER TABLE "students" ADD COLUMN     "img_path" VARCHAR(255);

c0158744051e5e827afaf6ee7b9373d4a46013e8
neophiles <mnv.tagle@gmail.com>
2026-04-25 02:47:11 +0800

    refactor(services): throw LANDMARK_LOCKED error when student tries to open unvisited landmark

--- backend/src/services/landmarks.service.ts
+++ backend/src/services/landmarks.service.ts
@@ -39,12 +39,7 @@ async function fetchLandmark(studentNum: string, landmarkId: number) {
   const { qr_string, ...publicData } = landmark;
 
   if (!visit) {
-    const { fun_fact, ...lockedData } = publicData;
-    
-    return {
-      ...lockedData,
-      is_unlocked: false,
-    };
+    throw new Error("Scan landmark QR first");
   }
 
   return {

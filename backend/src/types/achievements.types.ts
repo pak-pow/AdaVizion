5dfebd9b52f9ca7056a3280246dfad2eb8d8a23a
neophiles <mnv.tagle@gmail.com>
2026-04-28 22:17:11 +0800

    refactor(types): omit updated_at in SeedAchievement

--- backend/src/types/achievements.types.ts
+++ backend/src/types/achievements.types.ts
@@ -1,6 +1,6 @@
 import type { Achievement } from "../../generated/prisma/client"
 
-type SeedAchievement = Omit<Achievement, "achievement_id" | "created_at">;
+type SeedAchievement = Omit<Achievement, "achievement_id" | "created_at" | "updated_at">;
 
 export type {
   SeedAchievement
